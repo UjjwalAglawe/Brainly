c923ba56fd25c148e3a809fd0f4543268d51d8e0
Ujjwal Aglawe <127344565+UjjwalAglawe@users.noreply.github.com>
2025-01-20 22:55:07 +0530

    Update Signin.tsx

--- Brainly-frontend/src/pages/Signin.tsx
+++ Brainly-frontend/src/pages/Signin.tsx
@@ -1,7 +1,6 @@
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import axios from "axios";
-import { BACKEND_URL } from "../config";
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from 'react-toastify';
