892b98f0a4e3d59f66c2865dd344a3f0546169bd
Ujjwal Aglawe <aglaweujjwal@gmail.com>
2025-01-20 23:45:04 +0530

    Last

--- Brainly/src/index.ts
+++ Brainly/src/index.ts
@@ -16,9 +16,8 @@ const app = express();
 const JWT_SECRET2=process.env.JWT_SECRET!;
 app.use(express.json());
 app.use(cors({
-    origin:['https://brainly-frontend-pi.vercel.app'],
+    origin:['https://brainly-73hz.vercel.app/'],
     methods:["POST","GET","DELETE"],
-    credentials:true
 }));
 
 app.options('*', cors());
