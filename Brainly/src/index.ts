ec95b7ab8594569b7d092893c9d8487b2c0441e9
Ujjwal Aglawe <127344565+UjjwalAglawe@users.noreply.github.com>
2025-01-21 09:54:33 +0530

    Update index.ts

--- Brainly/src/index.ts
+++ Brainly/src/index.ts
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(cors({
     origin:['https://brainly-73hz.vercel.app/'],
     methods:["POST","GET","DELETE"],
+    credentials:true,
 }));
 
 app.options('*', cors());
