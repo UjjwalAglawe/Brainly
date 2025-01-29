f21661ca69b7e58580c5f7ab2c865cebcc2b4e92
Ujjwal Aglawe <127344565+UjjwalAglawe@users.noreply.github.com>
2025-01-29 22:18:44 +0530

    Update index.ts

--- Brainly/src/index.ts
+++ Brainly/src/index.ts
@@ -16,12 +16,14 @@ const app = express();
 const JWT_SECRET2=process.env.JWT_SECRET!;
 app.use(express.json());
 app.use(cors({
-    origin:['https://brainly-73hz.vercel.app/'],
-    methods:["POST","GET","DELETE"],
-    credentials:true,
+    origin: ['https://brainly-73hz.vercel.app'],
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 
-app.options('*', cors());
+
+
 
 app.post("/api/v1/signin", async (req, res) => {
     const schema = z.object({
@@ -282,5 +284,5 @@ app.get("/api/v1/brain/share/:shareLink", async (req, res) => {
     })
 })
 
-
+app.options('*', cors());
 app.listen(3000);
